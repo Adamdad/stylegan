d79b0e717b9db3448ca3312318c3bbc6ad4050aa
Adamdad <787707188@qq.com>
2019-11-27 03:20:00 -0800

    package

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /ceph/xyang/Text2image/code/stylegan
 nvidia-smi
-conda env create -f environment.yml
-source activate yxy
+source activate
+pip install tensorflow-gpu=1.14.0
 python train.py
